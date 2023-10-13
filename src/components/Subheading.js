876fe149a95c0ea05a9576f38ca2ef69083cd911
agusm <agusmena60@gmail.com>
2023-10-13 10:53:14 -0300

    readme changes

---
+++ src/components/Subheading.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Subheading extends Component {
+    render() {
+        return (
+           <p className='text-center lead'>Lista de tareas usando react</p>
+        );
+    }
+}
+
+export default Subheading;
